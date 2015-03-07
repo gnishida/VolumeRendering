bb4ec10581df886407a635b9264858055f7a6508
gnishida <gnishida@purdue.edu>
2015-03-07 12:25:37 -0500

    update gitignore

--- VolumeRendering/GeneratedFiles/ui_MainWindow.h
+++ VolumeRendering/GeneratedFiles/ui_MainWindow.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MainWindow.ui'
 **
-** Created: Sat Mar 7 11:05:06 2015
-**      by: Qt User Interface Compiler version 4.8.2
+** Created: Sat Mar 7 12:22:34 2015
+**      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
